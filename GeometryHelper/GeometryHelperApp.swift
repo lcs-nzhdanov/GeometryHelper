95c8c87dd073a223d1fd9c56faeab36cd6d49dc4
Nikita Zhdanov <nzhdanov@lcs.on.ca>
2024-01-10 12:00:54 -0500

    Initial Commit

---
+++ GeometryHelper/GeometryHelperApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeometryHelperApp.swift
+//  GeometryHelper
+//
+//  Created by Nikita Zhdanov on 2024-01-10.
+//
+
+import SwiftUI
+
+@main
+struct GeometryHelperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
