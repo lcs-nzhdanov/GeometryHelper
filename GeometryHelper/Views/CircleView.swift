0533be12251c676e62d4f56f7d0833e94e28c8d1
Nikita Zhdanov <nzhdanov@lcs.on.ca>
2024-01-10 12:09:55 -0500

    Organized project by adding Views and Model groups and a new view file named CircleView.

---
+++ GeometryHelper/Views/CircleView.swift
@@ -0,0 +1,18 @@
+//
+//  CircleView.swift
+//  GeometryHelper
+//
+//  Created by Nikita Zhdanov on 2024-01-10.
+//
+
+import SwiftUI
+
+struct CircleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CircleView()
+}
