9a03b75d0491487e419ee29d83b6dcc94f4bee6b
Nikita Zhdanov <nzhdanov@lcs.on.ca>
2024-01-10 12:17:40 -0500

    Added a stored property and a Text view to show the radius.

--- GeometryHelper/Views/CircleView.swift
+++ GeometryHelper/Views/CircleView.swift
@@ -8,8 +8,14 @@
 import SwiftUI
 
 struct CircleView: View {
+    //MARK: Stored Properties
+    @State var radius = 10.0
+    
+    
+    //MARK: Computed Properties
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("\(radius)")
+            .font(.largeTitle)
     }
 }
 
